61035c03693bbe10bbe5e533081abd74355b09dd
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-08 11:45:01 -0700

    work on adding todo

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+const form = document.querySelector("#new-todo-form");
+const todoInput = document.querySelector("#todo-input");
+const list = document.querySelector("#list");
+const template = document.querySelector("#list-item-template");
+
 // Add todos
-// User will type in todo and click add todo button.  This should then add the todo to the list above.
+// User will type in todo and click add todo button.
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const todoName = todoInput.value;
+  // render todo
+  renderTodo(todoName);
+  todoInput.value = "";
+});
+
+function renderTodo(todoName) {
+  const templateClone = template.content.cloneNode(true);
+  const textElement = templateClone.querySelector("[data-list-item-text]");
+  textElement.innerText = todoName;
+  list.appendChild(templateClone);
+}
+
+//This should then add the todo to the list above.
 
 // Delete todos
 // Complete todos
