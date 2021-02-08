9e159fb04b09c200634a01d2eb865b9be9a42771
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-08 11:30:19 -0700

    create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add todos
+// User will type in todo and click add todo button.  This should then add the todo to the list above.
+
+// Delete todos
+// Complete todos
+// Save todos
+// Load todos
